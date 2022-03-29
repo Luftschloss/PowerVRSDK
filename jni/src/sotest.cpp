4aa907a3d306a385bcee2736ad8348201ef0d4c4
yufei <15201225365@163.com>
2022-03-29 10:17:46 +0800

    gpu_counter_api_test

--- jni/src/sotest.cpp
+++ jni/src/sotest.cpp
@@ -1,12 +1,110 @@
 #include "sotest.h"
-#include "../3rd/include/PVRScopeStats.h"
+#include <string.h>
 
 extern "C" {
 
+	inline const char* ToString(EPVRScopeInitCode v)
+	{
+		switch (v)
+		{
+			case ePVRScopeInitCodeOk:   return "ePVRScopeInitCodeOk";
+			case ePVRScopeInitCodeOutOfMem:   return "ePVRScopeInitCodeOutOfMem";
+			case ePVRScopeInitCodeDriverSupportNotFound: return "ePVRScopeInitCodeDriverSupportNotFound";
+			case ePVRScopeInitCodeDriverSupportInsufficient: return "ePVRScopeInitCodeDriverSupportInsufficient";
+			case ePVRScopeInitCodeDriverSupportInitFailed: return "ePVRScopeInitCodeDriverSupportInitFailed";
+			case ePVRScopeInitCodeDriverSupportQueryInfoFailed: return "ePVRScopeInitCodeDriverSupportQueryInfoFailed";
+		}
+	}
+
     int Init()
     {
-        SPVRScopeImplData* data;
-        int re = PVRScopeInitialise(&data);
-        return re;
+		reading.pfValueBuf = nullptr;
+		reading.nValueCnt = 0;
+		reading.nReadingActiveGroup = 99;
+		EPVRScopeInitCode re = PVRScopeInitialise(&scopeData);
+		LOGI("InitResult: %s", ToString(re));
+		if (scopeData == 0)
+			LOGI("Null scopeData");
+		else
+			LOGI("scopeData：%d", sizeof(*scopeData));
+        return (int)re;
     }
+
+	int UpdateCounterList()
+	{
+		int re = PVRScopeGetCounters(scopeData, &numCounter, &counters, &reading);
+		if (re)
+		{
+			LOGI("UpdateCounterList %i ", numCounter); // numCounter为0
+		}
+		else
+		{
+			LOGI("UpdateCounterList Faild");
+			numCounter = 0;
+		}
+		return numCounter;
+	}
+
+    char* GetPVRScopeGetDescription()
+    {
+        return (char*)PVRScopeGetDescription();
+    }
+
+	void SetGroup(unsigned int group)
+	{
+		PVRScopeSetGroup(scopeData, group);
+		activeGroup = group;
+	}
+
+    SPVRScopeTimingPacket* GetPVRScopeReadTimingData(unsigned int* const pnCount)
+    {
+        const struct SPVRScopeTimingPacket* timePacket;
+        timePacket = PVRScopeReadTimingData(scopeData, pnCount);
+        return (SPVRScopeTimingPacket*)timePacket;
+    }
+
+    unsigned int GetCounter(EPVRScopeStandardCounter standardCounter)
+    {
+        unsigned int counterNum;
+		unsigned int counterIdx;
+		unsigned int readCounter = 0;
+		
+		if (scopeData)
+		{
+			SPVRScopeCounterReading* psReading = nullptr;
+			LOGD("GetCounter1");
+			psReading = &reading;
+			
+			if (PVRScopeReadCounters(scopeData, psReading) && psReading)
+			{
+				LOGD("GetCounter2");
+				readCounter = reading.nValueCnt;
+				// When the active group is changed, retrieve new indices
+				counterIdx = PVRScopeFindStandardCounter(counterNum, counters, activeGroup, standardCounter);
+				counterNum = reading.pfValueBuf[counterIdx];
+				numCounter = 25;
+				for (int i = 0; i < numCounter; ++i)
+				{
+					LOGI("Idx:%i-->%s", i, counters[i].pszName);
+					if (i < reading.nValueCnt)
+					{
+						////Print the 3D Load
+						//if (strcmp(counters[i].pszName, "GPU task load: 3D core") == 0)
+						//{
+						//	LOGD("%s : %f\%", psCounters[i].pszName, sReading.pfValueBuf[i]);
+						//}
+						////Print the TA Load
+						//else if (strcmp(counters[i].pszName, "GPU task load: TA core") == 0)
+						//{
+						//	
+						//	LOGD("%s : %f\%", psCounters[i].pszName, sReading.pfValueBuf[i]);
+						//}
+					}
+				}
+			}
+		}
+        return readCounter;
+    }
+
+
 }
--- jni/src/sotest.h
+++ jni/src/sotest.h
@@ -1,6 +1,16 @@
 #ifndef __SOTest_H__
 #define __SOTest_H__
 
+#include "../3rd/include/PVRScopeStats.h"
+#include <android/log.h>
+
+#define TAG "PVR" // 这个是自定义的LOG的标识   
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__)   // 定义LOGD类型   
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__)    // 定义LOGI类型   
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__)    // 定义LOGW类型   
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__)   // 定义LOGE类型   
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__)   // 定义LOGF类型 
+
 #if 0
 #define EXPORT_DLL __declspec(dllexport) //导出dll声明
 #else
@@ -9,7 +19,31 @@
 
 extern "C" {
 
+    SPVRScopeImplData* scopeData;
+
+    SPVRScopeCounterDef* counters;
+
+    SPVRScopeCounterReading reading;
+
+    /// <summary>
+    /// Counter数量
+    /// </summary>
+    unsigned int numCounter;
+    unsigned int activeGroup;
+
     EXPORT_DLL int Init();
+
+    EXPORT_DLL int UpdateCounterList();
+
+    EXPORT_DLL void SetGroup(unsigned int activeGroup);
+
+    EXPORT_DLL char* GetPVRScopeGetDescription();
+
+    EXPORT_DLL SPVRScopeTimingPacket* GetPVRScopeReadTimingData(unsigned int* const pnCount);
+
+    EXPORT_DLL unsigned int GetCounter(EPVRScopeStandardCounter standardCounter);
+
+	
 }
 
 #endif
